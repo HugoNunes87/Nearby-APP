df6d921cd36c9c34e538e3fce85649f9242a47f0
HugoNunes87 <hugolima87@hotmail.com>
2024-12-10 22:47:36 -0300

    Inico do projeto - telas de inicio

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native"
+
+import { Steps } from "@/components/steps"
+import { Welcome } from "@/components/welcome"
+import { Step } from "@/components/step"
+
+export default function Index() {
+  return (
+    <View style={{flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+      <Steps />
+    </View>
+
+  )
+}
